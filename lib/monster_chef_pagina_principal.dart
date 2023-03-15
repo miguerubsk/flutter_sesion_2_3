ac7d8625ea2462e670a9db382a372326e37df460
Miguerubsk <miguerubsk@gmail.com>
2023-03-15 20:24:03 +0100

    Create monster_chef_pagina_principal.dart

---
+++ lib/monster_chef_pagina_principal.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'lista_compra_pantalla.dart';
+import 'recetas_pantalla.dart';
+
+class MonsterChefPaginaPrincipal extends StatefulWidget {
+  const MonsterChefPaginaPrincipal({Key? key}) : super(key: key);
+
+  @override
+  State<MonsterChefPaginaPrincipal> createState() => _MonsterChefPaginaPrincipalState();
+}
+
+class _MonsterChefPaginaPrincipalState extends State<MonsterChefPaginaPrincipal> {
+  int _categoriaActiva = 0;
+  static var paginas = <Widget>[
+    ListaCompraPantalla(),
+    RecetasPantalla(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Monster Chef'),),
+      body: paginas[_categoriaActiva],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _categoriaActiva,
+        onTap: _alPulsar,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.list),
+            label: 'Lista compra',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.grid_view_rounded),
+            label: 'Recetas',
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _alPulsar(int indice) {
+    setState(() => _categoriaActiva = indice);
+  }
+}
