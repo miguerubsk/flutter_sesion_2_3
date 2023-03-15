ae927d77e1340e3ead973b2997a0e475ff714ab5
Miguerubsk <miguerubsk@gmail.com>
2023-03-15 20:24:26 +0100

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,13 +7,13 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
+import 'package:flutter_sesion_2_3/monster_chef_app.dart';
 import 'package:flutter_sesion_2_3/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const MonsterChefApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
