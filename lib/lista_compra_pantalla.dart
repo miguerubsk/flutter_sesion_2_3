15de1cd16c352519833325c630ad6e7d34840471
Miguerubsk <miguerubsk@gmail.com>
2023-03-15 20:23:43 +0100

    Create lista_compra_pantalla.dart

---
+++ lib/lista_compra_pantalla.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+
+class ListaCompraPantalla extends StatelessWidget {
+  const ListaCompraPantalla({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+        child: Text('Lista de la compra 🛒'),
+    );
+  }
+}
