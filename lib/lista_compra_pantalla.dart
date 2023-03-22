191552c740ad3e25d08367b181cc51a8fc0b8486
Miguerubsk <miguerubsk@gmail.com>
2023-03-22 15:23:47 +0100

    Update lista_compra_pantalla.dart

--- lib/lista_compra_pantalla.dart
+++ lib/lista_compra_pantalla.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_sesion_2_3/lista_compra_pantalla_vacia.dart';
 
 
 class ListaCompraPantalla extends StatelessWidget {
   const ListaCompraPantalla({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return const Center(
-        child: Text('Lista de la compra 🛒'),
+    return Scaffold(
+      body: const ListaCompraPantallaVacia(),
+      floatingActionButton: FloatingActionButton(
+        elevation: 0,
+        onPressed: () {
+        },
+        child: const Icon(Icons.add),
+      ),
     );
   }
 }
